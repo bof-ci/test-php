156b5fdfbd0b544b75a281b833175cf4d00b1124
vagrant <vagrant@homestead>
2018-07-30 21:46:59 +0100

    backup

---
+++ src/AppBundle/BusinessAnalyst.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nico
+ * Date: 30/07/18
+ * Time: 17:08
+ */
+
+namespace BOF\AppBundle;
+
+
+use function foo\func;
+
+class BusinessAnalyst
+{
+    private $results;
+    const JANUARY = 1;
+    const DECEMBER = 12;
+    const NA = 'n/a';
+
+    public $minYear;
+    public $maxYear;
+
+    public function setResults($results)
+    {
+        $this->results = $results;
+        return $this;
+    }
+
+    public function formatResult()
+    {
+        $output = [];
+        $minYear = $maxYear = (int) current($this->results)['year'];
+        foreach ($this->results as $result)
+        {
+            if ($result['year'] <= $minYear) {
+                $minYear = (int) $result['year'];
+            }
+
+            if ($result['year'] >= $maxYear) {
+                $maxYear = (int) $result['year'];
+            }
+
+            $output[$result['profile_name']][$result['year']][(int)$result['month']] = $result['views'];
+        }
+
+        $this->minYear = $minYear;
+        $this->maxYear = $maxYear;
+
+        return $this->fillMissingData($output);
+    }
+
+    public function fillMissingData(array $results)
+    {
+        foreach ($results as $person => &$result)
+        {
+            for ($year = $this->minYear; $year <= $this->maxYear; $year++) {
+                if (!isset($result[$year])) {
+                    $result[$year] = [];
+                }
+
+                $result[$year] = array_replace($this->fillAllYear(), $result[$year]);
+            }
+
+            ksort($result);
+        }
+
+        return $results;
+    }
+
+    private function fillAllYear()
+    {
+        return array_fill(self::JANUARY, self::DECEMBER, self::NA);
+    }
+
+    public function getHeader()
+    {
+        return array_merge(['Profile'], self::getMonths());
+    }
+
+    public static function getMonths()
+    {
+        $months = [];
+        for ($m = self::JANUARY; $m <= self::DECEMBER; ++$m){
+            $months[$m] = \DateTime::createFromFormat('!m', $m)->format('F');
+        }
+
+        return $months;
+    }
+
+    private function getProfiles(array $results)
+    {
+        return array_unique(array_column($results, 'profile_name'));
+    }
+}
--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace BOF\Command;
 
+use BOF\AppBundle\BusinessAnalyst;
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -13,20 +15,45 @@ class ReportYearlyCommand extends ContainerAwareCommand
     {
         $this
             ->setName('report:profiles:yearly')
-            ->setDescription('Page views report')
-        ;
+            ->setDescription('Page views report');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var $db Connection */
-        $io = new SymfonyStyle($input,$output);
+        $io = new SymfonyStyle($input, $output);
         $db = $this->getContainer()->get('database_connection');
 
-        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+        $profiles = $db->query(
+            'SELECT 
+                p.profile_id,
+                p.profile_name as profile_name,
+                DATE_FORMAT(v.date, \'%Y\') AS year,
+                DATE_FORMAT(v.date, \'%m\') AS month,
+                SUM(v.views) as views
+            FROM
+                profiles AS p
+                    JOIN
+                views AS v ON p.profile_id = v.profile_id
+            GROUP BY p.profile_id , p.profile_name , DATE_FORMAT(v.date, \'%Y\') , DATE_FORMAT(v.date, \'%m\')
+            ORDER BY p.profile_name , DATE_FORMAT(v.date, \'%Y\') , DATE_FORMAT(v.date, \'%m\')'
+        )->fetchAll();
 
-        // Show data in a table - headers, data
-        $io->table(['Profile'], $profiles);
+        $businessAnalyst = new BusinessAnalyst();
 
+        $profiles = $businessAnalyst
+            ->setResults($profiles)
+            ->formatResult();
+
+        for ($year = $businessAnalyst->minYear; $year <= $businessAnalyst->maxYear; $year++) {
+            $head = [];
+            $result = [];
+            for ($m = BusinessAnalyst::JANUARY; $m <= BusinessAnalyst::DECEMBER; ++$m){
+                $head[] = \DateTime::createFromFormat('!m', $m)->format('F');
+            }
+        }
+
+            // Show data in a table - headers, data
+//        $io->table($businessAnalyst->getHeader(), $profiles);
     }
 }
