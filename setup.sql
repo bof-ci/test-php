7a4d6b7dfe44e3df9540d334d9dfc326497d6a7d
Kravinskiy <mark.kravinskiy@gmail.com>
2018-08-06 03:43:16 +0200

    Updating SQL Schema

--- setup.sql
+++ setup.sql
@@ -8,20 +8,36 @@ GRANT USAGE ON * . * TO 'bof-test'@'localhost' IDENTIFIED BY 'bof-test' WITH MAX
 
 GRANT ALL PRIVILEGES ON `bof_test` . * TO 'bof-test'@'localhost' WITH GRANT OPTION ;
 
-DROP TABLE IF EXISTS `bof_test`.`profiles`;
-CREATE TABLE `bof_test`.`profiles` (
+
+DROP TABLE IF EXISTS `daily_statistics_views`;
+CREATE TABLE `daily_statistics_views` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `profile` int(11) unsigned NOT NULL,
+  `date` date NOT NULL,
+  `views` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `deleted` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `date` (`date`,`deleted`,`profile`),
+  KEY `profile` (`profile`),
+  CONSTRAINT `daily_statistics_views_ibfk_1` FOREIGN KEY (`profile`) REFERENCES `profiles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE `profiles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `profile_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL DEFAULT '',
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   `deleted` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `bof_test`.`profiles` WRITE;
-/*!40000 ALTER TABLE `bof_test`.`profiles` DISABLE KEYS */;
+LOCK TABLES `profiles` WRITE;
+/*!40000 ALTER TABLE `profiles` DISABLE KEYS */;
 
-INSERT INTO `bof_test`.`profiles` (`id`, `profile_name`, `created`, `updated`, `deleted`)
+INSERT INTO `profiles` (`id`, `name`, `created`, `updated`, `deleted`)
 VALUES
   (1,'Karl Lagerfeld','2017-01-01 00:00:00','2017-01-01 00:00:00','9999-12-31 00:00:00'),
   (2,'Anna Wintour','2017-01-01 00:00:00','2017-01-01 00:00:00','9999-12-31 00:00:00'),
@@ -29,36 +45,20 @@ VALUES
   (4,'Pierre Alexis Dumas','2017-01-01 00:00:00','2017-01-01 00:00:00','9999-12-31 00:00:00'),
   (5,'Sandra Choi','2017-01-01 00:00:00','2017-01-01 00:00:00','9999-12-31 00:00:00');
 
-/*!40000 ALTER TABLE `bof_test`.`profiles` ENABLE KEYS */;
+/*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-DROP TABLE IF EXISTS `bof_test`.`daily_statistics_views`;
-CREATE TABLE `bof_test`.`statistics_daily_views` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `profile_id` int(11) unsigned NOT NULL,
-  `date` date NOT NULL,
-  `views` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `deleted` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `date` (`date`,`deleted`,`profile_id`),
-  KEY `profile_id` (`profile_id`),
-  CONSTRAINT `statistics_daily_views_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `bof_test`.`profiles` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-DROP TABLE IF EXISTS `bof_test`.`views`;
-CREATE TABLE `bof_test`.`views` (
+DROP TABLE IF EXISTS `views`;
+CREATE TABLE `views` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `profile_id` int(11) unsigned NOT NULL,
+  `profile` int(11) unsigned NOT NULL,
   `date` datetime NOT NULL,
   `user_data` text,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   `deleted` datetime DEFAULT '9999-12-31 00:00:00',
   PRIMARY KEY (`id`),
-  KEY `profile_id` (`profile_id`),
-  CONSTRAINT `views_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `bof_test`.`profiles` (`id`)
+  KEY `profile` (`profile`),
+  CONSTRAINT `views_ibfk_1` FOREIGN KEY (`profile`) REFERENCES `profiles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
